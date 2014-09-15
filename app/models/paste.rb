160924e354c5e2bfc1bb42866339755255d293df
sharma1nitish <sharma1nitish@gmail.com>
2014-09-15 14:22:30 +0530

    Created model for Paste

---
+++ app/models/paste.rb
@@ -0,0 +1,24 @@
+# == Schema Information
+#
+# Table name: pastes
+#
+#  id         :integer          not null, primary key
+#  content    :string(255)
+#  token      :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#
+
+class Paste < ActiveRecord::Base
+
+  before_create :allot_token
+
+  validates_uniqueness_of :token
+  validates_presence_of :content
+  
+  private
+
+  def allot_token
+    self.token = SecureRandom.urlsafe_base64
+  end  
+end
